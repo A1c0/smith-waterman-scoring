e82cc956944f673c376227a5a762e408849a0084
Alexandre CONTASSOT* <alexandrecontassot@hotmail.fr>
2019-01-26 19:21:01 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+//en me basant de la documentaiotn suivante
+
+const data1 = ['C','G', 'C', 'T', 'A', 'G', 'T', 'C'];
+const data2 = ['C', 'A', 'T', 'C'];
+
+
---
+++ test/sub-test.js
@@ -0,0 +1,3 @@
+const sinon = require('sinon');
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
