948c4070402a2f895c2dd066e250486ab3f2aef5
Alexandre CONTASSOT* <alexandrecontassot@hotmail.fr>
2019-01-28 20:08:18 +0100

    Add sub function and test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-//en me basant de la documentaiotn suivante
+// En me basant de la documentation suivante
+// http://sequencage.outness.net/algo1.htm
 
-const data1 = ['C','G', 'C', 'T', 'A', 'G', 'T', 'C'];
-const data2 = ['C', 'A', 'T', 'C'];
-
+/* const data1 = ['C', 'G', 'C', 'T', 'A', 'G', 'T', 'C'];
+const data2 = ['C', 'A', 'T', 'C']; */
 
--- test/sub-test.js
+++ test/sub-test.js
@@ -1,3 +1,66 @@
-const sinon = require('sinon');
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
+
+chai.use(chaiAsPromised);
+chai.should();
+
+const sub = require('../lib/sub');
+
+describe('sub.js', () => {
+  describe('sub between 2 numbers', () => {
+    const a = 12.2;
+    const b = 11.3;
+    const c = 12.2;
+
+    it('should be equal to 1 when there are two different number', () => {
+      sub(a, b).should.equal(1);
+    });
+
+    it('should be equal to 0 when there are two same number', () => {
+      sub(a, c).should.equal(0);
+    });
+
+    it('should be equal to 1 when there are one number and one insert', () => {
+      sub(a, '-').should.equal(1);
+      sub('-', a).should.equal(1);
+    });
+  });
+
+  describe('sub between 2 objects', () => {
+    const a = {name: 'bob', age: 12};
+    const b = {name: 'marc', age: 16};
+    const c = {name: 'bob', age: 12};
+
+    it('should be equal to 1 when there are two different objects', () => {
+      sub(a, b).should.equal(1);
+    });
+
+    it('should be equal to 0 when there are two same object', () => {
+      sub(a, c).should.equal(0);
+    });
+
+    it('should be equal to 1 when there are one object and one insert', () => {
+      sub(a, '-').should.equal(1);
+      sub('-', a).should.equal(1);
+    });
+  });
+
+  describe('sub between 2 string', () => {
+    const a = 'hello';
+    const b = 'hi';
+    const c = 'hello';
+
+    it('should be equal to 1 when there are two different strings', () => {
+      sub(a, b).should.equal(1);
+    });
+
+    it('should be equal to 0 when there are two same string', () => {
+      sub(a, c).should.equal(0);
+    });
+
+    it('should be equal to 1 when there are one string and one insert', () => {
+      sub(a, '-').should.equal(1);
+      sub('-', a).should.equal(1);
+    });
+  });
+});
