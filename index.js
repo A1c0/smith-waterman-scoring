2dc5db246c735114c740625108c461807659fd20
Alexandre CONTASSOT* <alexandrecontassot@hotmail.fr>
2019-02-03 15:06:54 +0100

    Correct Travis integration

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
-// En me basant de la documentation suivante
-// http://sequencage.outness.net/algo1.htm
-
-/* const data1 = ['C', 'G', 'C', 'T', 'A', 'G', 'T', 'C'];
-const data2 = ['C', 'A', 'T', 'C']; */
+module.exports = require('./app/smith-waterman-score');
 
