9c0321671df5014b275ca34b19061aba9cfaf1b9
Alexandre CONTASSOT* <alexandrecontassot@hotmail.fr>
2019-02-04 18:20:35 +0100

    Correct JSDoc to show

--- app/smith-waterman-score.js
+++ app/smith-waterman-score.js
@@ -4,13 +4,14 @@ const LocalAligner = require('../lib/local-aligner');
  * Compute score this two strings
  * @param {array} arrayQ A sequence
  * @param {array} arrayP An another sequence
- * @param {number} gap The score gain to do a gap
- * @param {number} mismatch The score gain to do a mismatch
- * @param {number} match The score gain to do a match
- * @returns {{score: {number}, finalQ: {array}, finalP: {array}}} The result
+ * @param {Object} option the options
+ * @param {number} option.gap The score gain to do a gap
+ * @param {number} option.mismatch The score gain to do a mismatch
+ * @param {number} option.match The score gain to do a match
+ * @returns {{score: number, finalQ: array, finalP: array}} The result
  */
-const smtihWaltermanScore = (arrayQ, arrayP, {gap, mismatch, match}) => {
-  const sws = new LocalAligner(arrayQ, arrayP, {gap, mismatch, match});
+const smtihWaltermanScore = (arrayQ, arrayP, option) => {
+  const sws = new LocalAligner(arrayQ, arrayP, option);
   sws.calcTables();
   sws.calcAlignemnt();
   return {finalQ: sws.finalQ, finalP: sws.finalP, score: sws.maxScore};
